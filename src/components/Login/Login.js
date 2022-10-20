5d6b509015e08a55139a0bc439706cf31dc06eeb
Talha Khalid <hafizmtalhakhalid@gmail.com>
2022-10-20 17:08:00 +0500

    log in

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import React from 'react';
 import styled  from 'styled-components';
 import bg from '../images/bg.jpg';
@@ -9,7 +10,7 @@ export const Login = () => {
     display: flex;
     justify-content: center;
     align-items: center;
-    background-image: url(${bg});
+    background: linear-gradient(rgba(0,0,0, .6), rgba(0,0,0, .6)) ,url(${bg});
     background-position: center;
     background-size: cover;
     width: 100%;
@@ -49,9 +50,75 @@ export const Login = () => {
   width: 19rem;
   height: 2.2rem;
   border: 1px solid #ddd;
-  background-color: #eaedef;
-  
+  background-color: #eaedef;  
+  padding: 0rem 1rem;
+  font-weight: bold;
   `
+  const Warn = styled.p`
+  color: red;
+  font-size: .7rem;
+  display: none;
+  `
+  const Inf= styled.p`
+  color: #bbb;
+  font-size: .7rem;
+  `
+  const Dave4 = styled.div`
+  display: flex;
+  width: 21rem;
+  justify-content: space-between;
+  `
+  const Dave5 = styled.div`
+  display:flex;
+  gap: .3rem;
+  align-items: center;
+  `
+  const Boxi= styled.input`
+  border-radius: 1rem;
+  `
+  const Span= styled.span`
+  font-weight: 500;
+  font-size: .85rem;
+  `
+  const A = styled.a`
+  text-decoration: none;
+  color: #bbb;
+  font-size: .8rem;
+  `
+  const Button= styled.button`
+  margin-top: 2.5rem;
+  width: 7rem;
+  height: 2.3rem;
+  background-color: #5E9044;
+  border: none;
+  outline: none;
+  color: #fff;
+  font-weight: 600 ; 
+ `
+  const Par = styled.p`
+  font-size: .8rem;
+  color: #bbb;
+  margin-top: 1rem;
+  `
+  const Ab = styled.a`
+  text-decoration: none;
+  font-weight: 500;
+  color: #5E9044;
+  `
+
+  const [values, setValues] = useState({
+    email:"",
+    password: "",
+})
+
+
+  const handleOnSubmit = (e)=>{
+    e.preventDefault();
+  }
+
+  const handleOnChange = (event)=>{
+    setValues({ ...values, event.target.value});
+  };
 
   return (
     <Maine>
@@ -62,18 +129,26 @@ export const Login = () => {
                 <h2>Welcome Back</h2>
                 <Para>Sign in with your email address and password</Para>
               </div>
-              <form>
+              <form onSubmit={handleOnSubmit}>
                 <Dave3>
                   <Label>Email Address</Label>
-                  <Input type="email"  />
+                  <Input onChange={handleOnChange} value={values} type="email" pattern='/^[a-zA-Z0-9]+@[a-zA-Z]+\.[A-Za-z]+$/'/>
+                  <Warn>Please enter a valid email</Warn>
                 </Dave3>
                 <Dave3>
                   <Label>Password</Label>
-                  <Input type="password" />
+                  <Input onChange={handleOnChange}  value={values} type="text" pattern='^(?=.{8,})(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+*!=]).*$' />
+                  <Inf>Password should be One letter, one number, one special character and 8 digit long</Inf>
                 </Dave3>
-                <div>
-                  
-                </div>
+                <Dave4>
+                  <Dave5>
+                  <Boxi type="checkbox" />
+                    <Span>Remember me</Span>
+                  </Dave5>
+                  <A href="#">Forgot Password?</A>
+                </Dave4>
+                <Button >Sign In</Button>
+                <Par>Don't have an account?<Ab href="#">Sign Up</Ab></Par>
               </form>
             </Dave2>
             <div>
